9cb09eb95d2c3a3c7621af9bb826e1ae52664115
Louis Rossouw <LRossouw@GenRe.com>
2020-11-02 02:23:36 +0200

    Add canadian model update script

---
+++ update_ca_covid_19_model.R
@@ -0,0 +1,23 @@
+# Update model to website.
+
+# copy data
+file_list = list.files(path = "../covid19model/Canada/results/",
+                       pattern = "^projections-mob-base-canada-",
+                       full.names = TRUE)
+
+file.copy(from = file_list,
+          to = "covid-19/",
+          overwrite = TRUE)
+
+file.copy(from = "../covid19model/modelling_covid-19_in_canada_at_a_provincial_level.html",
+          to = "covid-19/",
+          overwrite = TRUE)
+
+# do git stuff
+source("git_tools.R")
+message = paste0("Update Canada COVID-19 Model ", Sys.time())
+path = "."
+git_add(path, path_to_add = "covid-19/modelling_covid-19_in_canada_at_a_provincial_level.html")
+git_add(path, path_to_add = "covid-19/projections-mob-base-canada*")
+git_commit(path, message)
+git_push(path)
